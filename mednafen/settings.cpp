e7f6228103f29e4c459b23f0e8e9d3686d3a9618
twinaphex <libretro@gmail.com>
2014-06-04 00:06:22 +0200

    (PCE Fast) 1 line at the bottom was missing

--- mednafen/settings.cpp
+++ mednafen/settings.cpp
@@ -83,7 +83,7 @@ uint64 MDFN_GetSettingUI(const char *name)
    if (!strcmp("pce_fast.slstart", name))
       return 0;
    if (!strcmp("pce_fast.slend", name))
-      return 230;
+      return 231;
 #elif defined(WANT_WSWAN_EMU)
    if (!strcmp("wswan.ocmultiplier", name))
       return 1;
